6f406cd4c5a4209dfaac2f421e1b1826624c0978
Mario <mariosanchezgamboa@hotmail.com>
2024-09-20 22:03:27 -0600

    Se crea UI FrmRegistrarAdministrador

---
+++ TiendaDeportiva/FrmRegistrarAdministrador.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace TiendaDeportiva
+{
+    partial class FrmRegistrarAdministrador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido1 = new System.Windows.Forms.TextBox();
+            this.txtApellido2 = new System.Windows.Forms.TextBox();
+            this.dtpFechaIngreso = new System.Windows.Forms.DateTimePicker();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido1 = new System.Windows.Forms.Label();
+            this.lblApellido2 = new System.Windows.Forms.Label();
+            this.lblFechaNacimiento = new System.Windows.Forms.Label();
+            this.lblFechaIngreso = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(385, 20);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(115, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Registrar Administrador";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(388, 82);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(200, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // dtpFechaNacimiento
+            // 
+            this.dtpFechaNacimiento.Location = new System.Drawing.Point(388, 264);
+            this.dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            this.dtpFechaNacimiento.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaNacimiento.TabIndex = 2;
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.Location = new System.Drawing.Point(255, 400);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(75, 23);
+            this.btnAtras.TabIndex = 3;
+            this.btnAtras.Text = "Atrás";
+            this.btnAtras.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(513, 400);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 4;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(388, 123);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 20);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtApellido1
+            // 
+            this.txtApellido1.Location = new System.Drawing.Point(388, 170);
+            this.txtApellido1.Name = "txtApellido1";
+            this.txtApellido1.Size = new System.Drawing.Size(200, 20);
+            this.txtApellido1.TabIndex = 7;
+            // 
+            // txtApellido2
+            // 
+            this.txtApellido2.Location = new System.Drawing.Point(388, 217);
+            this.txtApellido2.Name = "txtApellido2";
+            this.txtApellido2.Size = new System.Drawing.Size(200, 20);
+            this.txtApellido2.TabIndex = 8;
+            // 
+            // dtpFechaIngreso
+            // 
+            this.dtpFechaIngreso.Location = new System.Drawing.Point(388, 310);
+            this.dtpFechaIngreso.Name = "dtpFechaIngreso";
+            this.dtpFechaIngreso.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaIngreso.TabIndex = 9;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(262, 130);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 10;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblApellido1
+            // 
+            this.lblApellido1.AutoSize = true;
+            this.lblApellido1.Location = new System.Drawing.Point(262, 177);
+            this.lblApellido1.Name = "lblApellido1";
+            this.lblApellido1.Size = new System.Drawing.Size(76, 13);
+            this.lblApellido1.TabIndex = 11;
+            this.lblApellido1.Text = "Primer Apellido";
+            // 
+            // lblApellido2
+            // 
+            this.lblApellido2.AutoSize = true;
+            this.lblApellido2.Location = new System.Drawing.Point(262, 220);
+            this.lblApellido2.Name = "lblApellido2";
+            this.lblApellido2.Size = new System.Drawing.Size(90, 13);
+            this.lblApellido2.TabIndex = 12;
+            this.lblApellido2.Text = "Segundo Apellido";
+            // 
+            // lblFechaNacimiento
+            // 
+            this.lblFechaNacimiento.AutoSize = true;
+            this.lblFechaNacimiento.Location = new System.Drawing.Point(262, 271);
+            this.lblFechaNacimiento.Name = "lblFechaNacimiento";
+            this.lblFechaNacimiento.Size = new System.Drawing.Size(108, 13);
+            this.lblFechaNacimiento.TabIndex = 13;
+            this.lblFechaNacimiento.Text = "Fecha de Nacimiento";
+            // 
+            // lblFechaIngreso
+            // 
+            this.lblFechaIngreso.AutoSize = true;
+            this.lblFechaIngreso.Location = new System.Drawing.Point(262, 317);
+            this.lblFechaIngreso.Name = "lblFechaIngreso";
+            this.lblFechaIngreso.Size = new System.Drawing.Size(90, 13);
+            this.lblFechaIngreso.TabIndex = 14;
+            this.lblFechaIngreso.Text = "Fecha de Ingreso";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(262, 85);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(84, 13);
+            this.lblId.TabIndex = 15;
+            this.lblId.Text = "ID Administrador";
+            // 
+            // FrmRegistrarAdministrador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.lblFechaIngreso);
+            this.Controls.Add(this.lblFechaNacimiento);
+            this.Controls.Add(this.lblApellido2);
+            this.Controls.Add(this.lblApellido1);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.dtpFechaIngreso);
+            this.Controls.Add(this.txtApellido2);
+            this.Controls.Add(this.txtApellido1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnAtras);
+            this.Controls.Add(this.dtpFechaNacimiento);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "FrmRegistrarAdministrador";
+            this.Text = "FrmRegistrarAdministrador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.DateTimePicker dtpFechaNacimiento;
+        private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido1;
+        private System.Windows.Forms.TextBox txtApellido2;
+        private System.Windows.Forms.DateTimePicker dtpFechaIngreso;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido1;
+        private System.Windows.Forms.Label lblApellido2;
+        private System.Windows.Forms.Label lblFechaNacimiento;
+        private System.Windows.Forms.Label lblFechaIngreso;
+        private System.Windows.Forms.Label lblId;
+    }
+}
