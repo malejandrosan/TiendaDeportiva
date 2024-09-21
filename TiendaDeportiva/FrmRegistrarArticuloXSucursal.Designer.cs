e146021ac0abf76ed570747905a39056b431d6fb
Mario <mariosanchezgamboa@hotmail.com>
2024-09-20 22:58:22 -0600

    Se crea UI FrmRegistrarArticuloXSucursal

---
+++ TiendaDeportiva/FrmRegistrarArticuloXSucursal.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace TiendaDeportiva
+{
+    partial class FrmRegistrarArticuloXSucursal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSucursal = new System.Windows.Forms.Label();
+            this.lblArticulo = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.cmbSucursal = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(310, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registrar Artículos por Sucursal";
+            // 
+            // lblSucursal
+            // 
+            this.lblSucursal.AutoSize = true;
+            this.lblSucursal.Location = new System.Drawing.Point(263, 87);
+            this.lblSucursal.Name = "lblSucursal";
+            this.lblSucursal.Size = new System.Drawing.Size(48, 13);
+            this.lblSucursal.TabIndex = 1;
+            this.lblSucursal.Text = "Sucursal";
+            // 
+            // lblArticulo
+            // 
+            this.lblArticulo.AutoSize = true;
+            this.lblArticulo.Location = new System.Drawing.Point(263, 180);
+            this.lblArticulo.Name = "lblArticulo";
+            this.lblArticulo.Size = new System.Drawing.Size(44, 13);
+            this.lblArticulo.TabIndex = 2;
+            this.lblArticulo.Text = "Artículo";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(263, 137);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(49, 13);
+            this.lblCantidad.TabIndex = 3;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.Location = new System.Drawing.Point(266, 392);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(75, 23);
+            this.btnAtras.TabIndex = 4;
+            this.btnAtras.Text = "Atrás";
+            this.btnAtras.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(484, 392);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 5;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // cmbSucursal
+            // 
+            this.cmbSucursal.FormattingEnabled = true;
+            this.cmbSucursal.Location = new System.Drawing.Point(363, 87);
+            this.cmbSucursal.Name = "cmbSucursal";
+            this.cmbSucursal.Size = new System.Drawing.Size(139, 21);
+            this.cmbSucursal.TabIndex = 6;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(363, 130);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(139, 20);
+            this.txtCantidad.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(266, 214);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(304, 150);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // FrmRegistrarArticuloXSucursal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.cmbSucursal);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnAtras);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblArticulo);
+            this.Controls.Add(this.lblSucursal);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmRegistrarArticuloXSucursal";
+            this.Text = "FrmRegistrarArticuloXSucursal";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSucursal;
+        private System.Windows.Forms.Label lblArticulo;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.ComboBox cmbSucursal;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
